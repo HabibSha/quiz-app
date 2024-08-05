61fe29f4f9ab59374c1dc634948a7ee64aa3ea2e
habibsha <habibshahalahi7424@gmail.com>
2024-08-05 23:52:13 +0600

    Answer component

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -7,9 +7,7 @@ const Answers = () => {
           className="form-checkbox h-[14px] w-[14px]cursor-pointer text-black"
           style={{ outline: "none", boxShadow: "none" }}
         />
-        <span className="text-black font-semibold">
-          I agree to the terms & conditions
-        </span>
+        <span className="text-black font-semibold">Pick the first answer</span>
       </label>
       <label className="bg-gray-200 px-5 py-4 flex items-center space-x-2 mb-3 cursor-pointer">
         <input
